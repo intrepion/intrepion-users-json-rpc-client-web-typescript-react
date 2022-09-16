4fcfabe8db9b19dea0c1fef20bcb230b59d68444
Oliver Forral <intrepion@gmail.com>
2022-09-15 22:24:18 -0600

    Ran prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // App.js
-import React, { useState, useEffect } from 'react';
-import axios, {AxiosError} from 'axios';
+import React, { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
 
-import './App.css';
+import "./App.css";
 
 // The REST API endpoint
-const SERVER_URL = process.env.REACT_APP_SERVER_URL ?? 'http://localhost:3000';
+const SERVER_URL = process.env.REACT_APP_SERVER_URL ?? "http://localhost:3000";
 
 interface GreetingJsonRpcResult {
   id: string;
@@ -25,13 +25,14 @@ const App = () => {
 
   // Define the function that fetches the data from API
   const fetchData = async () => {
-    axios.post<GreetingJsonRpcResult>(SERVER_URL)
-      .then(response => {
+    axios
+      .post<GreetingJsonRpcResult>(SERVER_URL)
+      .then((response) => {
         setGreeting(response.data.result.greeting);
         setLoading(false);
       })
-      .catch(error => {
-        const err = error as AxiosError
+      .catch((error) => {
+        const err = error as AxiosError;
         setError(err.message);
         setLoading(false);
       });
@@ -45,9 +46,7 @@ const App = () => {
   return (
     <div className="wrapper">
       {!loading ? (
-        <div className="content">
-          {greeting}
-        </div>
+        <div className="content">{greeting}</div>
       ) : (
         <p className="loading">Loading... </p>
       )}
